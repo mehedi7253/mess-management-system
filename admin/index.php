25115c5d81cf65bab409b7b075230d925befd3bb
Mehedi Hasan <46008075+mehedi7253@users.noreply.github.com>
2022-03-09 21:49:00 +0600

    final

--- admin/index.php
+++ admin/index.php
@@ -36,67 +36,31 @@
 
             <!-- Icon Cards-->
             <div class="row">
-                <div class="col-xl-4 col-sm-6 mb-3">
-                    <div class="card text-white bg-primary o-hidden h-100">
-                        <div class="card-body">
-                            <div class="card-body-icon">
-                                <i class="fas fa-fw fa-comments"></i>
-                            </div>
-                            <div class="mr-5">
-                              
-                            </div>
-                        </div>
-                        <a class="card-footer text-white clearfix small z-1" href="manage-member.php">
-                            <span class="float-left">Total Meal</span>
-                            <span class="float-right">
-                  <i class="fas fa-angle-right"></i>
-                </span>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-xl-4 col-sm-6 mb-3">
-                    <div class="card text-white bg-secondary o-hidden h-100">
-                        <div class="card-body">
-                            <div class="card-body-icon">
-                                <i class="fas fa-fw fa-list"></i>
-                            </div>
-                            <div class="mr-5">
-                               
-                            </div>
-                        </div>
-                        <a class="card-footer text-white clearfix small z-1" href="manage-booking.php">
-                            <span class="float-left">Total Bazar</span>
-                            <span class="float-right">
-                  <i class="fas fa-angle-right"></i></span></a>
-                    </div>
-                </div>
-
-                <div class="col-xl-4 col-sm-6 mb-3">
-                    <div class="card text-white bg-info o-hidden h-100">
-                        <div class="card-body">
-                            <div class="card-body-icon">
-                                <i class="fas fa-fw fa-list"></i>
-                            </div>
-                            <div class="mr-5">
-                              
-                            </div>
-                        </div>
-                        <a class="card-footer text-white clearfix small z-1" href="tour-booking.php">
-                            <span class="float-left">Month</span>
-                            <span class="float-right">
-                  <i class="fas fa-angle-right"></i></span></a>
-                    </div>
-                </div>
-
-
                
+                <div class="col-md-10 mx-auto">
+                   <div class="table-responsive">
+                       <table class="table table-bordered">
+                           <thead>
+                               <tr>
+                                   <th>Item Name</th>
+                                   <th>Item Price</th>
+                               </tr>
+                           </thead>
+                           <tbody>
+                               <?php
+                                $sql = "SELECT * FROM weakly_bazars";
+                                $res = mysqli_query($connect, $sql);
+                                while($row = mysqli_fetch_assoc($res)){?>
+                                    <td><?php echo $row['item_name'];?></td>
+                                    <td><?php echo number_format($row['price'],2);?></td>
+                                <?php }?>
+                               <tr>
 
-
-                <div class="col-xl-4 col-sm-6 mb-3">
-                   
+                               </tr>
+                           </tbody>
+                       </table>
+                   </div>
                 </div>
-
 
             </div>
         </div>
